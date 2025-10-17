4ad6226fae0658bc5cbd2000aa7cd5925e17315d
michal-mm <michal@Magic.local>
2025-10-17 11:29:30 +0200

    Small code refactoring - renaming, consolidating methods, extracting... improving code cleanness and readability

--- src/main/java/com/michal_mm/tools/shorts_scraper/ShortsScraper.java
+++ src/main/java/com/michal_mm/tools/shorts_scraper/ShortsScraper.java
@@ -22,9 +22,11 @@ public class ShortsScraper {
     private static final String YOUTUBE_API_BASE = "https://www.googleapis.com/youtube/v3";
     private static final String GOOGLE_API_KEY = "GOOGLE_API_KEY";
     private static final String OUTPUT_FORMATER_KEY = "output.formatter";
+    public static final String PLAYLIST_ITEMS_ENDPOINT = "/playlistItems?part=snippet&maxResult=50";
+    public static final String YT_SHORTS_LIST_PREFIX = "UUSH";
 
-    private static String API_KEY;
-    private static String OUTPUT_FORMATTER;
+    private static String apiKey;
+    private static String outputFormatter;
 
     static void main(String[] args) throws IOException {
         initProperties();
@@ -36,36 +38,36 @@ public class ShortsScraper {
 
         String channelId = args[0];
 
-        List<VideoItem> shorts = findShorts(channelId);
-        formatResponse(shorts, channelId);
-    }
+        List<VideoItem> shorts = fetchAllShorts(channelId);
 
-    private static void formatResponse(List<VideoItem> shorts, String channelId) {
-        OutputFormatterFactory.outputFormatter(OUTPUT_FORMATTER)
+        OutputFormatterFactory.outputFormatter(outputFormatter)
                 .formatResponse(shorts, channelId);
     }
 
-    private static List<VideoItem> findShorts(String channelId) throws IOException {
+    private static List<VideoItem> fetchAllShorts(String channelId) throws IOException {
         List<VideoItem> listOfShorts = new ArrayList<>();
         String pageToken = null;
+        // replace first UC from channel Id with "UUSH" to get shorts playlist
+        String shortsPlaylistId = YT_SHORTS_LIST_PREFIX + channelId.substring(2);
 
         do {
-            String urlListItems = YOUTUBE_API_BASE + "/playlistItems?part=snippet&channelId=" + channelId +
-                    "&maxResults=" + 50 +
-                    // replace first UC from channel Id with "UUSH" to get shorts playlist
-                    "&playlistId=" + "UUSH" + channelId.substring(2) +
-                    "&order=date&type=video&key=" + API_KEY;
+            String urlListItems = buildApiUrl(channelId, shortsPlaylistId, pageToken);
 
-            if (pageToken != null) {
-                urlListItems += "&pageToken=" + pageToken;
-            }
-
-            var response = makeApiRequestAndReturnJsonString(urlListItems);
+            var response = fetchJson(urlListItems);
             pageToken = parsePageTokenFromJsonResponse(response);
             listOfShorts.addAll(buildListOfShorts(response));
         } while (pageToken != null);
 
         return listOfShorts;
+    }
+
+    private static String buildApiUrl(String channelId, String shortsPlaylistId, String pageToken) {
+        String urlListItems = YOUTUBE_API_BASE + PLAYLIST_ITEMS_ENDPOINT +
+                "&channelId=" + channelId +
+                "&playlistId=" + shortsPlaylistId +
+                "&order=date&type=video&key=" + apiKey;
+
+        return pageToken != null ? urlListItems + "&pageToken=" + pageToken : urlListItems;
     }
 
     private static List<VideoItem> buildListOfShorts(String videosJsonStr) {
@@ -75,11 +77,11 @@ public class ShortsScraper {
 
     private static void initProperties() {
         ConfigStore.loadConfiguration("app.properties");
-        API_KEY = ConfigStore.stringValue(GOOGLE_API_KEY);
-        OUTPUT_FORMATTER = ConfigStore.stringValueOrDefault(OUTPUT_FORMATER_KEY, OutputFormatterFactory.SIMPLE);
+        apiKey = ConfigStore.stringValue(GOOGLE_API_KEY);
+        outputFormatter = ConfigStore.stringValueOrDefault(OUTPUT_FORMATER_KEY, OutputFormatterFactory.SIMPLE);
     }
 
-    private static String makeApiRequestAndReturnJsonString(String urlString) throws IOException {
+    private static String fetchJson(String urlString) throws IOException {
         URL url = URI.create(urlString).toURL();
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
@@ -87,11 +89,11 @@ public class ShortsScraper {
         conn.setRequestProperty("Accept", "application/json");
 
         if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            conn.disconnect();
             throw new RuntimeException("HTTP error code: " + conn.getResponseCode());
         }
 
         String videosJsonStr = new InputStreamReader(conn.getInputStream()).readAllAsString();
-
         conn.disconnect();
 
         return videosJsonStr;
