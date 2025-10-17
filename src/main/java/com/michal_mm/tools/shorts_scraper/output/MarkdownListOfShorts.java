2df1fd1ff95227ce513f82b973cc599ba4cf73d2
michal-mm <michal@Magic.local>
2025-10-17 11:12:53 +0200

    Markdown formatter small fixes

--- src/main/java/com/michal_mm/tools/shorts_scraper/output/MarkdownListOfShorts.java
+++ src/main/java/com/michal_mm/tools/shorts_scraper/output/MarkdownListOfShorts.java
@@ -4,19 +4,21 @@ import com.michal_mm.tools.shorts_scraper.model.VideoItem;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MarkdownListOfShorts implements ListOfShorts{
     @Override
     public void formatResponse(List<VideoItem> shorts, String channelId) {
         Collections.reverse(shorts);
+        AtomicInteger index = new AtomicInteger(1);
 
         IO.println("### YouTube Shorts for channel: ==" + channelId + "==");
 
-        IO.println("| - | # | TITLE | URL |");
-        IO.println("|---|-----|-------------|----|");
+        IO.println("| # | TITLE | URL |");
+        IO.println("|-----|-----------------|------------------|");
 
         for(VideoItem video : shorts) {
-            IO.println("| [ ] | 1. | " +
+            IO.println("| " + index.getAndIncrement() + ". | " +
                     video.title() + " | " +
                     "[" + video.url() + "](" + video.url() + ")|");
         }
