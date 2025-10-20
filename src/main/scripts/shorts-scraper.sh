560e9c8f22a104590e3c5abb008b8049ea4c0c4f
michal-mm <michal@Magic.local>
2025-10-20 17:21:16 +0200

    Added sh script along with distribution target in pom.xml
    One can get a ready to use directory by running mvn package and copying the directory Shorts-Scraper-App into any place, editing app.properties and giving ti a try

---
+++ src/main/scripts/shorts-scraper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -e app.properties ]
+then
+	java -jar "$DIR/ShortsScraper.jar" "$@"
+else
+	echo "File app.properties not found"
+	echo "Consider running:"
+	echo "cp example.app.properties app.properties"
+	echo "and edit it before running Shorts Scraper again"
+fi
