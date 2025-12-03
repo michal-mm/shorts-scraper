284adaa7cad1aac44f31b51f710dc01294a114a8
michal-mm <michal.miotk@gmail.com>
2025-12-03 17:08:01 +0100

    Added simple scenarios to test calling main method without params (expected IllegalStateException) and with params expecting IOException from calling Google API.

---
+++ src/test/java/com/michal_mm/tools/shorts_scraper/ShortsScraperTest.java
@@ -0,0 +1,21 @@
+package com.michal_mm.tools.shorts_scraper;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ShortsScraperTest {
+
+    @Test
+    void testMain() {
+        String[] input = new String[0];
+        assertThrows(IllegalStateException.class, () -> ShortsScraper.main(input));
+    }
+
+    @Test
+    void testSimpleWithHttp400() {
+        assertThrows(IOException.class, () -> ShortsScraper.main(new String[] {"@java"}));
+    }
+}
